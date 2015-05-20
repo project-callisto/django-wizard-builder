bc0d2601085bc114a46e9f3ddffcfc4a90c8b378
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-05-19 22:32:36 -0700

    functional test + use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_anything(self):
+        self.assertTrue(False)
+
+    def test_can_start_a_report_and_submit_it(self):
+        #Edith visits Callisto
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mentions Callisto
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a report straight away
+
+        # She types "Something shady happened" into a text box
+
+        # When she hits enter, the page updates, and now the page lists
+        # "Report: Something shady happened" as an item in a list
+
+        # There is a button allowing her to submit. She submits the report
+
+        # The page updates again, and now shows the report as submitted
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
