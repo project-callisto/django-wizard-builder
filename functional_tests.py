99cbb4558124766b4b6502ced758f5ea068ead40
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-05-21 18:40:01 -0700

    functional test to enter a report

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -16,18 +17,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mentions Callisto
         self.assertIn('Callisto', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Callisto', header_text)
 
         # She is invited to enter a report straight away
+        inputbox = self.browser.find_element_by_id('id_new_report')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Write it down'
+        )
 
         # She types "Something shady happened" into a text box
+        inputbox.send_keys('Something shady happened')
 
         # When she hits enter, the page updates, and now the page lists
         # "Report: Something shady happened" as an item in a list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_report_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Report: Something shady happened' for row in rows)
+        )
 
         # There is a button allowing her to submit. She submits the report
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows the report as submitted
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
