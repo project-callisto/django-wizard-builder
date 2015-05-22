dffc89ad56fc8d40f728ed4f1f2fa10f76e09e8a
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-05-21 18:06:59 -0700

    first unit tests with dummy view & url mapping

--- crapllisto/urls.py
+++ crapllisto/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from reports import views
+
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'crapllisto.views.home', name='home'),
+    url(r'^$', views.home_page, name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
--- reports/tests.py
+++ reports/tests.py
@@ -1,6 +1,9 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from reports.views import home_page
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(3 * 3, 4)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
