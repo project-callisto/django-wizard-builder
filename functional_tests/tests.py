cd23e3e83535ad011af53efdd73037174fbc447c
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-05-27 13:34:54 -0700

    hack FT runner to be able to run against staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,8 +2,23 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
+import sys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +34,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_can_start_a_report_and_submit_it(self):
         #Edith visits Callisto
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mentions Callisto
         self.assertIn('Crapllisto', self.browser.title)
@@ -62,7 +77,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # Francis visits the home page. No sign of Edith's report
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Something shady happened', page_text)
         self.assertNotIn('Another shady thing went down', page_text)
@@ -84,7 +99,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the report input box is nicely centered
