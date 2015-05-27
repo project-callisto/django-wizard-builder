1ee1e03cdd655a61f17c750f26cc5ce24e6a3556
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-05-26 22:42:50 -0700

    disable admin & configure collectstatic

--- crapllisto/settings.py
+++ crapllisto/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'django.contrib.admin',
+    #'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -105,3 +105,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, '../static'))
