ab4765943c0d15a7975dd17dcdb5b98204456bf0
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-06-18 10:00:50 -0700

    add signup functional test

---
+++ functional_tests/test_login.py
@@ -0,0 +1,37 @@
+from .base import FunctionalTest
+from selenium.webdriver.support.ui import WebDriverWait
+
+class LoginTest(FunctionalTest):
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id)
+        )
+
+    def test_login(self):
+        # Edith goes to the awesome Callisto site
+        # and notices a "Sign in" link for the first time.
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_signup').click()
+
+        # A login screen appears
+        edith_signup_url = self.browser.current_url
+        self.assertRegex(edith_signup_url, '/signup/')
+
+        # Edith signs up with her email address
+        self.browser.find_element_by_id(
+            'authentication_email'
+        ).send_keys('edith@notrealemail.com')
+        self.browser.find_element_by_id(
+            'password'
+        ).send_keys('password')
+        self.browser.find_element_by_id(
+            'password_confirmation'
+        ).send_keys('password')
+        self.browser.find_element_by_tag_name('button').click()
+
+        # She can see that she is logged in
+        self.wait_for_element_with_id('id_logout')
+        # She is on an interstitial page explaining record
+        encouragement = self.browser.find_element_by_id('.encouragement')
+        self.assertIn("Now we'll begin", encouragement.text)
