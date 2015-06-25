e22c869d8c586609b7457a39a9014f63565326a7
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-06-25 12:47:06 -0700

    don't use cdn for bootstrap & jQuery when developing locally

--- config/settings/local.py
+++ config/settings/local.py
@@ -33,3 +33,18 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+BOOTSTRAP3 = {
+
+    # The URL to the jQuery JavaScript file
+    'jquery_url': '/static/js/jquery.min.js',
+
+    #    # The complete URL to the Bootstrap CSS file (None means derive it from base_url)
+    'css_url': '/static/bootstrap/css/bootstrap.min.css',
+
+    # The complete URL to the Bootstrap CSS file (None means no theme)
+    'theme_url': '/static/bootstrap/css/bootstrap-theme.min.css',
+
+    # The complete URL to the Bootstrap JavaScript file (None means derive it from base_url)
+    'javascript_url': '/static/bootstrap/js/bootstrap.min.js',
+}
