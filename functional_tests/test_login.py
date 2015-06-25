90e2c2ce6df34667571a005ee9d0be140da4d647
Kelsey Gilmore-Innis <kelsey@sexualhealthinnovations.org>
2015-06-25 12:47:23 -0700

    FT for login

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,6 +1,8 @@
 from .base import FunctionalTest
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class LoginTest(FunctionalTest):
 
@@ -26,7 +28,7 @@ class LoginTest(FunctionalTest):
         self.browser.get(self.server_url)
         self.browser.find_element_by_id('id_signup').click()
 
-        # A login screen appears
+        # A sign up screen appears
         edith_signup_url = self.browser.current_url
         self.assertRegex(edith_signup_url, '/signup')
 
@@ -45,6 +47,48 @@ class LoginTest(FunctionalTest):
         # She can see that she is logged in
         self.wait_for_element_with_id('id_logout')
         self.assertRaises(NoSuchElementException, self.browser.find_element_by_id, 'id_signup')
+        #self.assertRaises(NoSuchElementException, self.browser.find_element_by_id, 'id_login')
+        # TK: what page do you go to after signup?
+
+        # Refreshing the page, she sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
+
+        # She clicks logout and is no longer logged in
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
+
+        # Logged out status also persists after refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
+
+    def test_login(self):
+        # Edith has been to the Callisto site before and created an account
+        User.objects.create(email='edith@secondvisit.com',
+                                   password="password")
+
+        # On visiting again, she sees a login link
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_login').click()
+
+        # A login screen appears
+        edith_signup_url = self.browser.current_url
+        self.assertRegex(edith_signup_url, '/login')
+
+        # Edith logs in
+        self.browser.find_element_by_id(
+            'id_username'
+        ).send_keys('edith@secondvisit.com')
+        self.browser.find_element_by_id(
+            'id_password'
+        ).send_keys('password')
+        self.browser.find_element_by_tag_name('button').click()
+
+        # She can see that she is logged in
+        self.wait_for_element_with_id('id_logout')
+        self.assertRaises(NoSuchElementException, self.browser.find_element_by_id, 'id_login')
+        self.assertRaises(NoSuchElementException, self.browser.find_element_by_id, 'id_signup')
         # TK: what page do you go to after login?
 
         # Refreshing the page, she sees it's a real session login,
@@ -62,5 +106,3 @@ class LoginTest(FunctionalTest):
 
 
     #TK: test sign up validation, including duplicate username
-    #TK: test login
-    #TK: test logout
